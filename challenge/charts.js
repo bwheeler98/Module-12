8e09870e3079830a541fbd9bbc61a86ad2d7cb25
bwheeler98 <100613516+bwheeler98@users.noreply.github.com>
2022-06-10 18:49:55 -0400

    Add files via upload

--- challenge/charts.js
+++ challenge/charts.js
@@ -139,8 +139,8 @@ function buildCharts(sample) {
     var gaugeLayout = { 
       title: "Belly Button Washing Frequency",
       width: 300,
-      height: 300,
-      margin: { t: 25, r: 25, l: 25, b: 25 },
+      height: 400,
+      margin: { t: 25, r: 25, l: 25, b: 25 }
     };
 
     Plotly.newPlot("gauge", gaugeData, gaugeLayout);
